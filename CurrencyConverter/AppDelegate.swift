185834ea45312804ef1d67db8e89d8064c49c78a
Luis Arias <lugearma@gmail.com>
2016-09-14 20:39:45 -0500

    was added navigation view controller

--- CurrencyConverter/AppDelegate.swift
+++ CurrencyConverter/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
         
+        let viewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: viewController)
         
         return true
     }
