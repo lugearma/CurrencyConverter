d6e0fabb4cf6b45e14445fd42d7aed102d232e24
Luis Arias <lugearma@gmail.com>
2016-09-14 21:07:37 -0500

    text field added

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -13,14 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.redColor()
+        self.view.backgroundColor = UIColor.whiteColor()
+        setupViews()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupViews() {
+        
+        let inputCurrency: UITextField = {
+            
+            let input = UITextField(frame: CGRect(x: 0.0, y: 100.0, width: 150.0, height: 60.0))
+            input.placeholder = "Currency I have"
+            input.borderStyle = UITextBorderStyle.RoundedRect
+            input.keyboardType = UIKeyboardType.NumberPad
+            input.translatesAutoresizingMaskIntoConstraints = false
+            
+            return input
+            
+        }()
+        
+        self.view.addSubview(inputCurrency)
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-30-[v0]-30-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": inputCurrency]))
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-100-[v0]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": inputCurrency]))
+        
     }
-
-
 }
 
